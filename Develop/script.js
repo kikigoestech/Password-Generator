e7f0b418f37aa21dc343f84725c5a4bd91f7f966
kikigoestech <iamtummo13>
2022-06-28 14:50:12 -0400

    for loop

--- Develop/script.js
+++ Develop/script.js
@@ -11,18 +11,24 @@ function generatePassword() {
 // 3. Generate password based on criteria
 // 4. Display password to the page
 
-
+// making sure click works 
     return "Generated Password"; 
 }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
+ function writePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -33,8 +39,10 @@ var password=document.getElementById("password");
         document.getElementById("password").value = password;
  
 
- var passwordLength = window.prompt("Choose password length 8-10");
+ var passwordLength = window.prompt("Choose password length 8-16");
  var characters = window.prompt("Would you like your password to include uppercase letters?")
  var characters = window.prompt("Would you like your password to include lowercase letters?")
  var characters = window.prompt("Would you like your password to include numbers?")
  var characters = window.prompt("Would you like your password to include special characters?")
+
+ 
